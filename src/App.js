3c213465269cf4c7e83333739ffd406882c05ebc
kishan2930 <kishambaliya@gmail.com>
2024-02-26 18:18:43 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import "./styles.css";
+import { useState } from "react";
+
+export default function App() {
+  const [count, setCount] = useState(0);
+  const handleIncrement = () => {
+    setCount((prev) => prev + 1);
+  };
+  const handleDecrement = () => {
+    setCount((prev) => prev - 1);
+  };
+  return (
+    <div>
+      <h1>Counter App</h1>
+      <p>Count: {count}</p>
+      <button onClick={handleIncrement}>Increment</button>
+      <button onClick={handleDecrement}>Decrement</button>
+    </div>
+  );
+}
